466b5b71df5976ac6c29cb6a8869c61149aef47f
hnefatafl01 <kevin.erick01@gmail.com>
2017-12-31 12:01:01 -0700

    greeting and new user alert

--- server/server.js
+++ server/server.js
@@ -11,20 +11,37 @@ var io = socketIO(server);
 
 io.on('connection', (socket) => {
     console.log('connected to client- new user connected');
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to nes chat!',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined chat',
+        createdAt: new Date().getTime()
+    });
 
     socket.on('createMessage', (message) => {
         console.log('created message: ', message);
-        io.emit('newMessage', {
-            text: message.text,
+        // io.emit('newMessage', {
+        //     text: message.text,
+        //     from: message.from,
+        //     createdAt: new Date().getTime()
+        // });
+
+        socket.broadcast.emit('newMessage', {
             from: message.from,
+            text: message.from,
             createdAt: new Date().getTime()
         });
-    })
+    });
 
     socket.on('disconnect', () => {
         console.log('client disconnected');
-    })
-})
+    });
+});
 
 app.use(express.static(path.join(__dirname, '../public')));
 
